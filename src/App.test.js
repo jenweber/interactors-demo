01b4e683fbe78d7b61dd8f1705d4cdc158ef23c8
Jen Weber <j@jenweber.me>
2020-11-19 20:44:33 -0500

    Using interactors

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, fireEvent } from '@testing-library/react';
 import App from './App';
+import { Button } from 'bigtest';
 
 test('regular jest and react testing', () => {
   const { getByText, queryByText } = render(<App />);
@@ -9,10 +10,9 @@ test('regular jest and react testing', () => {
   expect(getByText('Sign out')).toBeTruthy();
 });
 
-test('using bigtest interactors', () => {
-  const { getByText, queryByText } = render(<App />);
-  expect(queryByText('Sign outd')).toBeFalsy();
-  fireEvent.click(getByText('Sign in'));
-  expect(queryByText('Sign in')).toBeFalsy();
-  expect(getByText('Sign out')).toBeTruthy();
+test('using bigtest interactors', async () => {
+  render(<App />);
+  await Button('Sign in').click();
+  await Button('Sign in').absent();
+  await Button('Sign out').exists();
 });
